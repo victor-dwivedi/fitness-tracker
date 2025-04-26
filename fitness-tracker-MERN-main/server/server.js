82416c2f3df5fa4e0494740aa88823f735489209
RISHIKESH DWIVEDI <129958222+victor-dwivedi@users.noreply.github.com>
2025-04-27 03:04:22 +0530

    Update server.js

--- fitness-tracker-MERN-main/server/server.js
+++ fitness-tracker-MERN-main/server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "fitness-tracker-MERN-main\server\build\index.html")));
 }
 
 // app.get('*', (req, res) => {
